6e5f67b53e4e16e727a795435f45c49f9b96ae3e
Kévin Dunglas <kevin@dunglas.fr>
2022-11-23 22:56:12 +0100

    test: fix CI

--- src/Authorization.php
+++ src/Authorization.php
@@ -27,10 +27,14 @@ final class Authorization
 
     private $registry;
     private $cookieLifetime;
+    /**
+     * @var Cookie::SAMESITE_*|null|null
+     */
     private $cookieSameSite;
 
     /**
-     * @param int|null $cookieLifetime in seconds, 0 for the current session, null to default to the value of "session.cookie_lifetime" or 3600 if "session.cookie_lifetime" is set to 0. The "exp" field of the JWT will be set accordingly if not set explicitly, defaults to 1h in case of session cookies.
+     * @param int|null                      $cookieLifetime in seconds, 0 for the current session, null to default to the value of "session.cookie_lifetime" or 3600 if "session.cookie_lifetime" is set to 0. The "exp" field of the JWT will be set accordingly if not set explicitly, defaults to 1h in case of session cookies.
+     * @param Cookie::SAMESITE_*|null       $cookieSameSite
      */
     public function __construct(HubRegistry $registry, ?int $cookieLifetime = null, ?string $cookieSameSite = Cookie::SAMESITE_STRICT)
     {
