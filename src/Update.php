2dd568932ae05e29865a0244a33ab125c7f52cd4
Thomas Landauer <thomas@landauer.at>
2025-01-17 11:34:38 +0100

    Update Update.php: Adding phpdoc
    Info is taken from https://github.com/symfony/symfony-docs/issues/20574#issuecomment-2597893282
    
    Why is the default value `null` and not `message`?

--- src/Update.php
+++ src/Update.php
@@ -34,6 +34,7 @@ final class Update
 
     /**
      * @param string|string[] $topics
+     * @param ?string $type SEE type: https://developer.mozilla.org/en-US/docs/Web/API/EventSource
      */
     public function __construct($topics, string $data = '', bool $private = false, ?string $id = null, ?string $type = null, ?int $retry = null)
     {
