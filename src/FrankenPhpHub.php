b6d3874001f326226d2c62015cef137056b3bf30
Kévin Dunglas <kevin@dunglas.fr>
2025-11-21 11:38:55 +0100

    Add support for FrankenPHP's mercure_publish() function (#129)
    * Add support for FrankenPHP's mercure_publish() function
    
    * Update src/Hub.php
    
    Co-authored-by: Jacob Dreesen <jacob@hdreesen.de>
    
    * cs
    
    * author
    
    * fix
    
    * static analysis
    
    * cs
    
    ---------
    
    Co-authored-by: Jacob Dreesen <jacob@hdreesen.de>

---
+++ src/FrankenPhpHub.php
@@ -0,0 +1,52 @@
+<?php
+
+/*
+ * This file is part of the Mercure Component project.
+ *
+ * (c) Kévin Dunglas <dunglas@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+declare(strict_types=1);
+
+namespace Symfony\Component\Mercure;
+
+use Symfony\Component\Mercure\Jwt\TokenFactoryInterface;
+
+/**
+ * @author Kévin Dunglas <kevin@dunglas.dev>
+ *
+ * @experimental
+ */
+final class FrankenPhpHub implements HubInterface
+{
+    public function __construct(
+        private readonly string $publicUrl,
+        private readonly ?TokenFactoryInterface $jwtFactory = null,
+    ) {
+    }
+
+    public function getPublicUrl(): string
+    {
+        return $this->publicUrl;
+    }
+
+    public function getFactory(): ?TokenFactoryInterface
+    {
+        return $this->jwtFactory;
+    }
+
+    public function publish(Update $update): string
+    {
+        return mercure_publish(
+            $update->getTopics(),
+            $update->getData(),
+            $update->isPrivate(),
+            $update->getId(),
+            $update->getType(),
+            $update->getRetry(),
+        );
+    }
+}
--- src/HubInterface.php
+++ src/HubInterface.php
@@ -14,7 +14,6 @@ declare(strict_types=1);
 namespace Symfony\Component\Mercure;
 
 use Symfony\Component\Mercure\Jwt\TokenFactoryInterface;
-use Symfony\Component\Mercure\Jwt\TokenProviderInterface;
 
 /**
  * @author Saif Eddin Gmati <azjezz@protonmail.com>
@@ -24,22 +23,12 @@ use Symfony\Component\Mercure\Jwt\TokenProviderInterface;
 interface HubInterface
 {
     /**
-     * Returns the Hub internal URL.
-     */
-    public function getUrl(): string;
-
-    /**
      * Returns the Hub public URL.
      *
-     * If the public URL is not configured, this method MUST return
-     * the internal URL {@see HubInterface::getUrl()}.
+     * If the public URL is not configured and the hub also implements the {@see RemoteHubInterface}, this method MUST return
+     * the internal URL {@see RemoteHubInterface::getUrl()}.
      */
     public function getPublicUrl(): string;
-
-    /**
-     * Return the token provider used by this Hub.
-     */
-    public function getProvider(): TokenProviderInterface;
 
     /**
      * Return the token factory associated with this Hub.
